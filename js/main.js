98b8eb6b0ed6d84a9e989e3a057bd6cc235d8ea8
OSAMA _NABIL <125906285+osamaghnam24@users.noreply.github.com>
2024-09-16 22:37:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function imgSlider(any) {
+    document.querySelector('.StarBucks').src = any;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
